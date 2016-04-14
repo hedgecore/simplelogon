de774bb0c55bf674d1b29b13ed7d5e46edf6c5ab
hedgecore <taylor.reed@outlook.com>
2016-04-14 02:01:06 -0700

    added migrations

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class BaseConfig(object):
     DEBUG = False
     SECRET_KEY = '\xc5zy\xd0\x813\xee\x97\x03\xec4l\xa0|\x9eYtg\x18\xe5\x8c\x8d\xc4s'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    print SQLALCHEMY_DATABASE_URI
 
 
 class DevelopmentConfig(BaseConfig):
