871cfc62f214abfaf824bdb019ecf36197823fa1
hedgecore <taylor.reed@outlook.com>
2016-04-14 20:41:17 -0700

    more blueprints

--- db_create.py
+++ db_create.py
@@ -1,13 +1,14 @@
-from app import db
-from models import BlogPost
+from project import db
+from project.models import User
 
 #create the database and the db tables
 db.create_all()
 
 # insert
-db.session.add(BlogPost("Good", "I\'m good."))
-db.session.add(BlogPost("well", "I\'m well."))
-db.session.add(BlogPost("postgres", "local postgres instance"))
+#db.session.add(BlogPost("Good", "I\'m good."))
+#db.session.add(BlogPost("well", "I\'m well."))
+#db.session.add(BlogPost("postgres", "local postgres instance"))
+
 
 
 #commit the changes
--- db_create_users.py
+++ db_create_users.py
@@ -1,9 +1,10 @@
-from app import db
-from models import User
+from project import db
+from project.models import User
 
 # insert data
-db.session.add(User("user1", "user1@notemail.com", "i'll-never-tell"))
+db.session.add(User("user1", "user1@notemail.com", "this is a password"))
 db.session.add(User("admin", "ad@min.com", "admin"))
+db.session.add(User("tom", "tom@tomstuff", "1qaz2wsx!QAZ@WSX"))
 
 # commit the changes
 db.session.commit()
